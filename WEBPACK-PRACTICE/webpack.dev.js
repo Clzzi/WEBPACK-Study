8f9eb1d91b69088207beb8dbdc7d6c03db144ca5
Clzzi <clzzi1109@naver.com>
2021-07-22 17:29:22 +0900

    Add: webpack.dev.js's compile option

--- WEBPACK-PRACTICE/webpack.dev.js
+++ WEBPACK-PRACTICE/webpack.dev.js
@@ -3,7 +3,23 @@ const common = require("./webpack.common");
 
 const config = {
   mode: "development",
+  devServer: {
+    open: false,
+    overlay: true,
+    port: 6969,
+    historyApiFallback: {
+      rewrites: [
+        {
+          from: /^\/subpage$/,
+          to: "subpage.html",
+        },
+        {
+          from: /./,
+          to: "404.html",
+        },
+      ],
+    },
+  },
 };
 
 module.exports = merge.merge(common, config);
-
